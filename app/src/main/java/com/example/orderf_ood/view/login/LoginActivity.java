51bec6807a500d7295316fc1399734e204052665
mi <dinhk@systena.co.jp>
2023-08-26 14:36:23 +0900

    add feature facebook login to tutorial and signin screen

--- app/src/main/java/com/example/orderf_ood/view/login/LoginActivity.java
+++ app/src/main/java/com/example/orderf_ood/view/login/LoginActivity.java
@@ -1,19 +1,31 @@
 package com.example.orderf_ood.view.login;
 
+import static com.example.orderf_ood.view.tutorial.TutorialActivity.accessToken;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ProgressBar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.orderf_ood.R;
 import com.example.orderf_ood.presenter.login.LoginPresenter;
+import com.example.orderf_ood.view.main.HomeActivity;
+import com.example.orderf_ood.view.tutorial.TutorialActivity;
+import com.facebook.AccessToken;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 public class LoginActivity extends AppCompatActivity implements ILoginActivity {
     private LoginPresenter mPresenter;
-
     private TabLayout mTabLayout;
     private ViewPager2 mViewPager;
 
@@ -33,7 +45,6 @@ public class LoginActivity extends AppCompatActivity implements ILoginActivity {
         // get
         mTabLayout = findViewById(R.id.tab_layout);
         mViewPager = findViewById(R.id.view_container_page);
-        //
         final LoginViewPagerAdapter pagerAdapter = new LoginViewPagerAdapter(getSupportFragmentManager(), getLifecycle());
         mViewPager.setAdapter(pagerAdapter);
         String[] listTitle = getResources().getStringArray(R.array.login_tab_title_array);
